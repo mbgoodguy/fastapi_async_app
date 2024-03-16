f98e8e11ab70f2b659a763739eb193bdaa1e92fe
mbgoodguy <nerooren4001@gmail.com>
2024-03-16 15:49:40 +0300

    Add notes schemas, db config

---
+++ src/app/api/notes.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from sqlalchemy.sql import crud
+
+from app.api.models import NoteSchema, NoteDB
+
+router = APIRouter()
+
+
+@router.post("/", response_model=NoteDB, status_code=201)
+async def create_note(payload: NoteSchema):
+    note_id = await crud.post(payload)
+
+    response_object = {
+        "id": note_id,
+        "title": payload.title,
+        "description": payload.description,
+    }
+
+    return response_object
--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 
 from app.api import ping
+from app.db import database, metadata, engine
+
+metadata.create_all(engine)
 
 app = FastAPI()
 app.include_router(ping.router)
 
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
